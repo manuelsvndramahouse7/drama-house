62d27680c54ddcc222989d456f0972dcf8f1f850
manuelsvndramahouse7 <anais.calat@icloud.com>
2025-12-23 22:38:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-const intro = document.getElementById("intro");
-const content = document.getElementById("content");
 const buttons = document.querySelectorAll(".vote-btn");
-const loserBox = document.getElementById("loser");
 
-// INTRO 2s
-setTimeout(() => {
-  intro.style.display = "none";
-  content.classList.remove("hidden");
-}, 2000);
-
-// AFFICHER PERDANT SI EXISTE
-const loser = localStorage.getItem("loser");
-if (loser !== null) {
-  loserBox.innerText = "❌ Le perdant est : Candidat " + (parseInt(loser) + 1);
+// si déjà voté
+if (localStorage.getItem("voted")) {
+  buttons.forEach(btn => {
+    btn.textContent = "VOTE ENREGISTRÉ";
+    btn.classList.add("disabled");
+    btn.disabled = true;
+  });
 }
 
-// VOTE
 buttons.forEach(btn => {
   btn.addEventListener("click", () => {
     if (localStorage.getItem("voted")) return;
 
     localStorage.setItem("voted", "true");
-    buttons.forEach(b => b.disabled = true);
 
-    btn.innerText = "VOTE ENREGISTRÉ";
+    buttons.forEach(b => {
+      b.textContent = "VOTE ENREGISTRÉ";
+      b.classList.add("disabled");
+      b.disabled = true;
+    });
   });
 });
