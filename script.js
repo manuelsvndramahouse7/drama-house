630bc0bd2bac8c6c73c9808d0a708c1ecf1c28c1
manuelsvndramahouse7 <anais.calat@icloud.com>
2025-12-23 14:44:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-setTimeout(() => {
-  document.getElementById("intro").style.display = "none";
-  document.getElementById("content").style.display = "block";
-}, 3000);
+// VOTE AU CLIC
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+
+    // Enlève le vote des autres
+    cards.forEach(c => c.classList.remove("active"));
+
+    // Active celle cliquée
+    card.classList.add("active");
+  });
+});
