c59f11bbb3aa900fd6898004763f15d85d2a4fad
manuelsvndramahouse7 <anais.calat@icloud.com>
2025-12-23 17:01:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-// FIN DE L’INTRO
-setTimeout(() => {
-  document.getElementById("intro").style.display = "none";
-  document.getElementById("content").style.display = "block";
-}, 2600);
-
-// ANIMATION DE VOTE
-document.querySelectorAll(".card").forEach(card => {
-  card.addEventListener("click", () => {
-    document.querySelectorAll(".card").forEach(c => c.classList.remove("active"));
-    card.classList.add("active");
-  });
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("app").classList.remove("hidden");
+  }, 2000);
 });
