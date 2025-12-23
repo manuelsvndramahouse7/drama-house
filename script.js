8cbd63a09bc3e88c347c13812b82c8136825910e
manuelsvndramahouse7 <anais.calat@icloud.com>
2025-12-23 12:05:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// rien pour l'instant
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    const intro = document.getElementById("intro");
+    const content = document.getElementById("content");
+
+    if (intro) intro.style.display = "none";
+    if (content) content.style.display = "block";
+  }, 3000);
+});
