2dad2862322f9f6108d2e35477ced3cff63ceaac
manuelsvndramahouse7 <anais.calat@icloud.com>
2025-12-23 16:22:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-// VOTE AU CLIC
-const cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-  card.addEventListener("click", () => {
-
-    // Enlève le vote des autres
-    cards.forEach(c => c.classList.remove("active"));
-
-    // Active celle cliquée
-    card.classList.add("active");
-  });
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  }, 2500); // durée de l’intro
 });
