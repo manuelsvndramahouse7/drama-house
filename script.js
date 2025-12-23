d7b9ac1a055be693e48fddde564245be420f4fe7
manuelsvndramahouse7 <anais.calat@icloud.com>
2025-12-23 14:20:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-window.addEventListener("load", () => {
-  const sound = document.getElementById("introSound");
-
-  // sécurité mobile (iPhone)
-  sound.volume = 0.8;
-
-  // joue le son à l'ouverture
-  sound.play().catch(() => {
-    console.log("Le son nécessite une interaction utilisateur");
-  });
-
-  // après l'intro (ex: 3 secondes)
-  setTimeout(() => {
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("content").style.display = "block";
-  }, 3000);
-});
-window.addEventListener("click", () => {
-  const sound = document.getElementById("introSound");
-  sound.play();
-}, { once: true });
+setTimeout(() => {
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}, 3000);
