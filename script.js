c8ccf5fd14663815e36c4eff7cb9de6a77d6b351
manuelsvndramahouse7 <anais.calat@icloud.com>
2025-12-23 17:14:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    document.getElementById("intro").style.display = "none";
-    document.getElementById("app").classList.remove("hidden");
-  }, 2000);
+// INTRO → DISPARITION APRÈS 2s
+setTimeout(() => {
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("app").classList.remove("hidden");
+}, 2000);
+
+// ANIMATION CLIC SUR CASE
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("click", () => {
+    card.classList.remove("clicked");
+    void card.offsetWidth;
+    card.classList.add("clicked");
+  });
 });
